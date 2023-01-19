cf8d01226bdd99d10af7ec773e581a163f9deaee
Levi <otterbach.levi@gmail.com>
2023-01-19 17:03:10 +0100

    evaluate points

--- src/main/java/de/unistuttgart/towercrushbackend/data/websockets/Game.java
+++ src/main/java/de/unistuttgart/towercrushbackend/data/websockets/Game.java
@@ -1,22 +1,58 @@
 package de.unistuttgart.towercrushbackend.data.websockets;
 
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Game {
+    private String lobbyName;
     private Set<Player> teamA;
     private Set<Player> teamB;
     private List<Round> rounds;
     private UUID configurationId;
     private int currentQuestionTeamA;
     private int currentQuestionTeamB;
+    @JsonIgnore
+    private long initialTowerSize;
     private int teamATowerSize;
     private int teamBTowerSize;
+    @JsonIgnore
+    private int teamAAnswerPoints;
+    @JsonIgnore
+    private int teamBAnswerPoints;
+    private String winnerTeam;
+    @JsonIgnore
+    private LocalDateTime startedGame;
+
+    private HashMap<String, Integer> correctAnswerCount;
+
+    public Game(final String lobbyName, final Set<Player> teamA, final Set<Player> teamB, final List<Round> rounds, final UUID configurationId, final long initialTowerSize) {
+        this.lobbyName = lobbyName;
+        this.teamA = teamA;
+        this.teamB = teamB;
+        this.rounds = rounds;
+        this.configurationId = configurationId;
+        this.currentQuestionTeamA = 0;
+        this.currentQuestionTeamB = 0;
+        this.initialTowerSize = initialTowerSize;
+        this.teamATowerSize = (int) initialTowerSize;
+        this.teamBTowerSize = (int) initialTowerSize;
+        this.teamAAnswerPoints = 0;
+        this.teamBAnswerPoints = 0;
+        this.winnerTeam = "";
+        this.startedGame = LocalDateTime.now();
+        this.correctAnswerCount = new HashMap<>();
+        this.correctAnswerCount.put("teamA", 0);
+        this.correctAnswerCount.put("teamB", 0);
+    }
 }
--- src/main/java/de/unistuttgart/towercrushbackend/service/websockets/GameService.java
+++ src/main/java/de/unistuttgart/towercrushbackend/service/websockets/GameService.java
@@ -2,18 +2,21 @@ package de.unistuttgart.towercrushbackend.service.websockets;
 
 import de.unistuttgart.towercrushbackend.data.Configuration;
 import de.unistuttgart.towercrushbackend.data.Question;
-import de.unistuttgart.towercrushbackend.data.websockets.Game;
-import de.unistuttgart.towercrushbackend.data.websockets.Player;
-import de.unistuttgart.towercrushbackend.data.websockets.Round;
-import de.unistuttgart.towercrushbackend.data.websockets.Vote;
+import de.unistuttgart.towercrushbackend.data.websockets.*;
 import de.unistuttgart.towercrushbackend.repositories.ConfigurationRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
 @Service
@@ -27,7 +30,21 @@ public class GameService {
     @Autowired
     ConfigurationRepository configurationRepository;
 
+    @Autowired
+    SimpMessagingTemplate simpMessagingTemplate;
+
+    @Autowired
+    WebsocketService websocketService;
+
+    ExecutorService executorService =
+        Executors.newFixedThreadPool(1);
+    Future<?> timeUpdate;
+
     private final Map<String, Game> games;
+    static final String LOBBY_TOPIC = "/topic/lobby/";
+    private static final int timePerQuestion = 10;
+    private static final int correctAnswerBonus = timePerQuestion / 2;
+    private static final int wrongAnswerMalus = -(timePerQuestion * 2);
 
     public GameService() {
         games = new ConcurrentHashMap<>();
@@ -44,25 +61,17 @@ public class GameService {
         }
         final Set<Player> teamA = lobbyManagerService.getLobby(lobby).getTeamA();
         final Set<Player> teamB = lobbyManagerService.getLobby(lobby).getTeamB();
-        final Game game = new Game(teamA, teamB, tempRounds, configurationId, 0, 0, tempRounds.size() * 10, tempRounds.size() * 10);
+        final Game game = new Game(
+            lobby,
+            teamA,
+            teamB,
+            tempRounds,
+            configurationId,
+            (long) tempRounds.size() * timePerQuestion);
         if (!games.containsKey(lobby)) {
             games.put(lobby, game);
         }
-    }
-
-    public void nextQuestion(final String lobby, final String team) {
-        final Game tempGame = games.get(lobby);
-        if (team.equals("teamA")) {
-            final int currentQuestionNumber = tempGame.getCurrentQuestionTeamA();
-            if (tempGame.getCurrentQuestionTeamA() < tempGame.getRounds().size()) {
-                tempGame.setCurrentQuestionTeamA(currentQuestionNumber + 1);
-            }
-        } else {
-            final int currentQuestionNumber = tempGame.getCurrentQuestionTeamB();
-            if (tempGame.getCurrentQuestionTeamB() < tempGame.getRounds().size()) {
-                tempGame.setCurrentQuestionTeamB(currentQuestionNumber + 1);
-            }
-        }
+        this.startTask();
     }
 
     public Game getGameForLobby(final String lobby) {
@@ -107,5 +116,112 @@ public class GameService {
             }
         }
         games.get(lobby).setRounds(rounds);
+    }
+
+    public void evaluateAnswers(final String lobby, final String team) {
+        final Game tempGame = games.get(lobby);
+        if (tempGame == null) {
+            log.error("evaluate answer but game doesnt exist, maybe ended");
+        }
+        if (team.equals("teamA")) {
+            final int currentQuestionNumber = tempGame.getCurrentQuestionTeamA();
+            final Map<String, Long> counts =
+                tempGame.getRounds().get(currentQuestionNumber).getTeamAVotes().stream().collect(Collectors.groupingBy(Vote::getAnswer, Collectors.counting()));
+            final String correctAnswer = tempGame.getRounds().get(currentQuestionNumber).getQuestion().getRightAnswer();
+            final long correctAnswerVotes = counts.get(correctAnswer);
+            counts.remove(correctAnswer);
+            int towerChange = correctAnswerBonus;
+            for (final Map.Entry<String, Long> entry : counts.entrySet()) {
+                if (entry.getValue() >= correctAnswerVotes) {
+                    towerChange = wrongAnswerMalus;
+                }
+            }
+            if (towerChange > 0) {
+                tempGame.getCorrectAnswerCount().put(team, tempGame.getCorrectAnswerCount().get(team) + 1);
+            }
+            tempGame.setTeamAAnswerPoints(tempGame.getTeamAAnswerPoints() + towerChange);
+        } else {
+            final int currentQuestionNumber = tempGame.getCurrentQuestionTeamB();
+            final Map<String, Long> counts =
+                tempGame.getRounds().get(currentQuestionNumber).getTeamBVotes().stream().collect(Collectors.groupingBy(Vote::getAnswer, Collectors.counting()));
+            final String correctAnswer = tempGame.getRounds().get(currentQuestionNumber).getQuestion().getRightAnswer();
+            final long correctAnswerVotes = counts.get(correctAnswer) == null ? 0 : counts.get(correctAnswer);
+            counts.remove(correctAnswer);
+            int towerChange = correctAnswerBonus;
+            for (final Map.Entry<String, Long> entry : counts.entrySet()) {
+                if (entry.getValue() >= correctAnswerVotes) {
+                    towerChange = wrongAnswerMalus;
+                }
+            }
+            if (towerChange > 0) {
+                tempGame.getCorrectAnswerCount().put(team, tempGame.getCorrectAnswerCount().get(team) + 1);
+            }
+            tempGame.setTeamBAnswerPoints(tempGame.getTeamBAnswerPoints() + towerChange);
+        }
+    }
+
+    public void nextQuestion(final String lobby, final String team) {
+        final Game tempGame = games.get(lobby);
+        if (team.equals("teamA")) {
+            final int currentQuestionNumber = tempGame.getCurrentQuestionTeamA();
+            if (tempGame.getCurrentQuestionTeamA() < tempGame.getRounds().size()) {
+                tempGame.setCurrentQuestionTeamA(currentQuestionNumber + 1);
+            } else {
+                setWinner(tempGame);
+            }
+        } else {
+            final int currentQuestionNumber = tempGame.getCurrentQuestionTeamB();
+            if (tempGame.getCurrentQuestionTeamB() < tempGame.getRounds().size()) {
+                tempGame.setCurrentQuestionTeamB(currentQuestionNumber + 1);
+            } else {
+                setWinner(tempGame);
+            }
+        }
+    }
+
+    private void setWinner(final Game tempGame) {
+        if (tempGame.getCorrectAnswerCount().get("teamA") > tempGame.getCorrectAnswerCount().get("teamB")) {
+            tempGame.setWinnerTeam("teamA");
+        } else if (tempGame.getCorrectAnswerCount().get("teamA") == tempGame.getCorrectAnswerCount().get("teamB")) {
+            tempGame.setWinnerTeam("draw");
+        } else {
+            tempGame.setWinnerTeam("teamB");
+        }
+    }
+
+    public void startTask() {
+        if (timeUpdate != null) {
+            return;
+        }
+        timeUpdate = executorService.submit(() -> {
+            boolean teamWon = false;
+            while (true) {
+                log.debug("update tower");
+                for (final Map.Entry<String, Game> entry : this.games.entrySet()) {
+                    final Game game = entry.getValue();
+                    game.setTeamATowerSize((int) (game.getTeamAAnswerPoints() + (game.getInitialTowerSize() - (ChronoUnit.SECONDS.between(game.getStartedGame(), LocalDateTime.now())))));
+                    game.setTeamBTowerSize((int) (game.getTeamBAnswerPoints() + (game.getInitialTowerSize() - (ChronoUnit.SECONDS.between(game.getStartedGame(), LocalDateTime.now())))));
+                    if (game.getTeamATowerSize() <= 0) {
+                        game.setWinnerTeam("teamB");
+                        teamWon = true;
+                    } else if (game.getTeamBTowerSize() <= 0) {
+                        game.setWinnerTeam("teamA");
+                        teamWon = true;
+                    }
+                    final UpdateGameMessage updateGameMessage = new UpdateGameMessage(game);
+                    final MessageWrapper updateLobbyMassageWrapped = websocketService.wrapMessage(
+                        updateGameMessage,
+                        Purpose.UPDATE_GAME_MESSAGE
+                    );
+                    simpMessagingTemplate.convertAndSend(GameService.LOBBY_TOPIC + game.getLobbyName(), updateLobbyMassageWrapped);
+                    if (teamWon) {
+                        log.debug("delete game");
+                        this.games.remove(game.getLobbyName());
+                        teamWon = false;
+                    }
+                }
+                Thread.sleep(1000);
+            }
+        });
     }
 }
