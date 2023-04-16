93ecc1c3f7fc20bad4a8bfbe8e6d03c5f468cae0
Gilian Rehm <st172032@stud.uni-stuttgart.de>
2023-04-16 11:53:52 +0200

    Define and throw a dedicated exception instead of using a generic one.

--- src/main/java/de/unistuttgart/towercrushbackend/controller/components/WebsocketListener.java
+++ src/main/java/de/unistuttgart/towercrushbackend/controller/components/WebsocketListener.java
@@ -123,7 +123,7 @@ public class WebsocketListener {
                     try {
                         Thread.sleep(1000);
                     } catch (final Exception e) {
-                        throw new RuntimeException(e);
+                        throw new InterruptedException();
                     }
                 }
             });
