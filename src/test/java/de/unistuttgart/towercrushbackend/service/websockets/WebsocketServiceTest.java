fe6080add12e974ba54949d0fd5265b21a88ffd0
Jonathan Scholz <contact@jonathan-scholz.de>
2023-04-19 16:40:17 +0200

    remove unused annotations from unit tests class

--- src/test/java/de/unistuttgart/towercrushbackend/service/websockets/WebsocketServiceTest.java
+++ src/test/java/de/unistuttgart/towercrushbackend/service/websockets/WebsocketServiceTest.java
@@ -1,26 +1,12 @@
 package de.unistuttgart.towercrushbackend.service.websockets;
 
-import de.unistuttgart.towercrushbackend.WireMockConfig;
 import de.unistuttgart.towercrushbackend.data.websockets.MessageWrapper;
 import de.unistuttgart.towercrushbackend.data.websockets.Purpose;
 import de.unistuttgart.towercrushbackend.data.websockets.UpdateGameMessage;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@AutoConfigureMockMvc
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@ActiveProfiles("test")
-@EnableConfigurationProperties
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = { WireMockConfig.class })
 public class WebsocketServiceTest {
     private WebsocketService websocketService;
 
